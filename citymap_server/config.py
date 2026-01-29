16059ec7a4777ee08e112711ceb2e24f8aa16a55
Leonid Briskindov <lb357@yandex.ru>
2026-01-29 21:09:29 +0300

    Update config.py

--- citymap_server/config.py
+++ citymap_server/config.py
@@ -10,7 +10,7 @@ VERSION = 26011
 def load_config():
     logging.info(f"Config loading...")
     try:
-        config = {}
+        config: dict = {}
         with open("config.json", "r", encoding="UTF-8") as file:
             config = json.loads(file.read())
         with open(".OFFICIAL_TOKEN", "r", encoding="UTF-8") as file:
@@ -18,8 +18,9 @@ def load_config():
         with open(".COOKIE_SECRET", "r", encoding="UTF-8") as file:
             cookie_secret = file.read()
         config["version"] = VERSION
+        address, port = config.pop("address"), config.pop("port")
         logging.info(f"Config loaded!")
-        return config, cookie_secret
+        return config, cookie_secret, address, port
     except Exception as exception:
         logging.warning(f"{exception} / Using default config...")
         return save_default_config()
@@ -29,7 +30,9 @@ def save_default_config():
         "official_place": "<REGION>",
         "official_name": "<DEPARTMENT>",
         "upload_max_size": 10485760,
-        "upload_extensions": [".jpg", ".jpeg", ".fbx", ".mp3", ".mp4", ".doc", ".docx", ".pdf", ".png"],
+        "upload_extensions": [".jpg", ".jpeg", ".png", ".fbx", ".mp3", ".mp4", ".doc", ".docx", ".pdf"],
+        "address": "0.0.0.0",
+        "port": 8888
     }
     with open("config.json", "w", encoding="UTF-8") as file:
         file.write(json.dumps(config, indent=4))
@@ -40,5 +43,6 @@ def save_default_config():
         cookie_secret = hasher.sha256(str(random.random()))+hasher.sha256(str(time.time()/2))
         file.write(cookie_secret)
     config["version"] = VERSION
+    address, port = config.pop("address"), config.pop("port")
     logging.info(f"Default config loaded!")
-    return config, cookie_secret
+    return config, cookie_secret, address, port
