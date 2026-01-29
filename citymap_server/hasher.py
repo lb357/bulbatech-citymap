d61205a4a0fbdd357cd75eb5457e6cfe79544f31
Leonid Briskindov <lb357@yandex.ru>
2026-01-29 16:51:53 +0300

    Update hasher.py

--- citymap_server/hasher.py
+++ citymap_server/hasher.py
@@ -2,8 +2,8 @@ import hashlib
 
 
 def sha256(s: str):
-    return hashlib.sha256(s.encode("ascii")).hexdigest()
+    return hashlib.sha256(s.encode("utf-8")).hexdigest()
 
 
 def md5(s: str):
-    return hashlib.md5(s.encode("ascii")).hexdigest()
+    return hashlib.md5(s.encode("utf-8")).hexdigest()
